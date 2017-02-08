7142cc272db7d0a8ec579b0a497da0d7dc5706a6
Anderhv <anderhv@stud.ntnu.no>
2017-02-08 16:17:08 +0100

    Add files via upload

---
+++ TTK4145_Gr_41/main.erl
@@ -0,0 +1,13 @@
+-module(main).
+-behaviour(gen_statem).
+
+-export([main/0]).
+
+
+main() ->
+
+	Namelist = ['elev1','elev2','elev3','elev4'],
+	ConnectionList = ['elev1@127.0.0.1','elev2@127.0.0.1','elev3@127.0.0.1','elev4@127.0.0.1'],
+
+	network:init_conn(ConnectionList,1),
+	network:init_listener(Namelist,1).
---
+++ TTK4145_Gr_41/network.erl
@@ -0,0 +1,49 @@
+-module(network).
+-export([init_conn/2,send_message/2,init_listener/2,read_mailbox/0,broadcast/2]).
+
+
+init_conn(_,5) ->
+	io:format("No node is available for connection~n");
+init_conn(ConnectionList, Element) ->
+	Connection = lists:nth(Element,ConnectionList),
+	io:format("Testing with connection: ~p ~n",[Connection]),
+	case 	pong == net_adm:ping(Connection) of
+			true ->	io:format("Successful connection~n");
+
+			false -> io:format("Failure to connect~n"),
+				init_conn(ConnectionList,Element+1)
+	end.
+
+
+read_mailbox() ->
+	receive
+		{msg, Msg} -> io:format("Received message: ~p~n",[Msg]);
+		{err, Err} -> io:format("Error message: ~p~n",[Err]);
+		{order, OrderMsg} -> io:format("Received order~n ~p",[OrderMsg]);
+			%handle order in scheduler
+			%implicit msg ack? if KnownReceivers < NumNodes : broadcast KnownReceivers
+		other -> io:format("Some other message retreived~n")
+	end,
+	read_mailbox().
+
+
+send_message(PID, Message) ->
+	PID ! {msg, Message}.
+
+
+broadcast(_,0) -> io:format("Message sent to all registered nodes.~n");
+broadcast(Message, Element) ->
+	NthPid = global:whereis_name(lists:nth(Element,global:registered_names())),
+	send_message(NthPid, Message),
+	broadcast(Message, Element-1).
+
+
+init_listener(Namelist, Element) ->
+	io:format("Testing with PID name: ~p ~n",[lists:nth(Element,Namelist)]),
+	case 	yes == global:register_name(lists:nth(Element,Namelist),spawn(network,read_mailbox,[])) of 
+			true ->
+				io:format("Successful registration ~n");
+			false ->
+				io:format("Name taken, attempting with different name~n"),
+				init_listener(Namelist, Element+1)
+	end.
